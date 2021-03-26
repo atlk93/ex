b2ccde0cb4afffbcd0fc4bb7c6cb2448826951bf
seohyun <dnw963@gmail.com>
2021-03-26 13:21:07 +0900

    커밋커밋

---
+++ DBManagerEx/DBManagerEx/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBManagerEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection sqlConn = new SqlConnection();
+        SqlCommand sqlCom = new SqlCommand();
+
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            dataGrid.Rows.Clear();          // db에서 클리어는 row먼저, 생성은 column먼저
+            dataGrid.Columns.Clear();
+
+            sbPanel1.Text = "DB File name";
+            sbPanel2.Text = "Table List";
+            sbPanel2.DropDownItems.Clear();
+            sbPanel3.Text = "Initialized";
+
+            sqlConn.Close();
+        }
+        //ㅅ정
+        private void mnuMigration_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+            string buf = sr.ReadLine();      // 첫번째 Line에는 각 Column의 Header Text가 들어가 있다
+            string[] sArr = buf.Split(',');  // ........ ','로 구분
+        }
+    }
+}
